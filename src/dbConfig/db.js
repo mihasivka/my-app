d7b4c5633c86bd5066f75a0262c41d1886a6d9f2
mihasivka <miha.koper@gmail.com>
2025-06-19 15:29:03 +0200

    Update db.js

--- src/dbConfig/db.js
+++ src/dbConfig/db.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 
 export async function connect() {
     try {
+        console.log('MONGO_URI:', process.env.MONGO_URI); // Add this line
         await mongoose.connect(process.env.MONGO_URI);
 
         mongoose.connection.on('connected', () => {
